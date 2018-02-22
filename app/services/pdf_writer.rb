3fbbe26c023156dadfb4ba35ba59164d6609c5b5
arup rakshit <aruprakshit@rocketmail.com>
2018-02-22 12:27:29 +0530

    fix pdfkit stylesheet and font loading issue

--- app/services/pdf_writer.rb
+++ app/services/pdf_writer.rb
@@ -1,7 +1,7 @@
 require 'render_anywhere'
 
 class PdfWriter
-  include RenderAnywhere
+  # include RenderAnywhere
 
   def initialize(name)
     @name = name
@@ -9,14 +9,19 @@ class PdfWriter
 
   def write_pdf
     kit = PDFKit.new(as_html)
+    # kit.stylesheets << Rails.root.join(
+    #   'public' + ActionController::Base.helpers.stylesheet_path('certificates_style')
+    # ).to_s
     kit.to_file("tmp/#{@name}_certificate.pdf")
   end
 
   private
 
   def as_html
-    render template: "certificates/pdf",
-    layout: 'certificate',
-    locals: { name: @name }
+    ApplicationController.render(
+      template: "certificates/pdf",
+      layout: 'certificate',
+      locals: { name: @name }
+    )
   end
 end
